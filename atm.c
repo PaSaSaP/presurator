71c42ec17214cb24cd390f769b2c468d82d1a852
pasasap <pasasap.gh@outlook.com>
2026-01-13 19:22:52 +0100

    Refactor calibration definitions and improve comments for sensor configuration

--- atm.c
+++ atm.c
@@ -3,9 +3,37 @@
 #include "atm.h"
 
 #define ATM_PIN A1
+
+// #define IDEAL_WORLD // Uncomment to use ideal world calibration
+#define SENSOR_BLACK // Uncomment to use SENSOR_BLACK calibration
+// #define SENSOR_GREEN // Uncomment to use SENSOR_GREEN calibration
+
+/* 
+MEAS_4_20_MA_MAX_ANALOG_READ means the maximum analog read value corresponding to 20mA
+which means that ideal sensor would have 15.79 ATM / 1.6MPa / 16 Bar pressure at 20mA
+MEAS_4_20_MA_MIN_ANALOG_READ means the minimum analog read value corresponding to 4mA
+which means that ideal sensor would have 0 ATM / 0MPa / 0 Bar pressure at 4mA
+
+Different sensors have different calibration values, hence the different defines.
+To calibrate your own sensor, measure the analog read value
+at 0 ATM and at 15.79 ATM and set the defines accordingly.
+*/
+
+#if defined(IDEAL_WORLD)
+// Ideal world calibration values
 #define MEAS_4_20_MA_MAX_ANALOG_READ (1023) // 10 bit ADC max value
 #define MEAS_4_20_MA_MIN_ANALOG_READ                                           \
   (MEAS_4_20_MA_MAX_ANALOG_READ * 4 / 20) // Min value for 4mA
+
+#elif defined(SENSOR_BLACK)
+#define MEAS_4_20_MA_MAX_ANALOG_READ (1060)
+#define MEAS_4_20_MA_MIN_ANALOG_READ (209)
+
+#elif defined(SENSOR_GREEN)
+#define MEAS_4_20_MA_MAX_ANALOG_READ (1084)
+#define MEAS_4_20_MA_MIN_ANALOG_READ (216)
+
+#endif
 
 static struct ATM atm_reading;
 
